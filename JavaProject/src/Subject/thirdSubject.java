f1231a522b043f7c823372ee55fe9235938409a9
YooKyungJung <yugyungs200@naver.com>
2017-09-25 20:40:13 +0900

    학년 클래스 버튼 연결 완료

---
+++ JavaProject/src/Subject/thirdSubject.java
@@ -0,0 +1,43 @@
+package Subject;
+
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Entrance.MirimEntrance;
+
+public class thirdSubject extends JFrame{
+	private JLabel label;
+	private JButton[] locationbtn = new JButton[3];
+	private String[] img = {"/location_1.png", "/location_2.png"};
+	Image[] btnimg = new Image[3];
+	public thirdSubject(){
+		//mbutton.setVisible(false);
+		setTitle("3ÇÐ³â");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1280, 750);
+		setLocationRelativeTo(null); //Ã¢ Áß¾Ó¿¡ ¶ç¿ì±â
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		contentPane.setLayout(null);
+		getContentPane().add(contentPane);
+		label = new JLabel("");
+		label.setSize(1280,720);
+		Image img = new ImageIcon(this.getClass().getResource("/Wallpaper.png")).getImage();
+		contentPane.add(label);
+		label.setIcon(new ImageIcon(img));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
