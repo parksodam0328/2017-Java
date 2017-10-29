20b385c175e32b0c12c41a5bf1fc4bdc1513bddc
Sodam Park <parksodam0228@e-mirim.hs.kr>
2017-10-30 07:54:31 +0900

    취업 데이터베이스 연동하여 출력하기 성공

---
+++ JavaProject/src/Employment/introEmployment.java
@@ -0,0 +1,101 @@
+package Employment;
+
+import java.awt.Image;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import static Subject.firstSubject.key;
+
+public class introEmployment extends JFrame{
+	private JLabel label;
+	private JPanel contentPane;
+	private JLabel introTitle;
+	private JLabel[] intro=new JLabel[4];
+	private JLabel[] dbIntro=new JLabel[4];
+	private Image[] introImg = new Image[4];
+	private String[] img = {"/introClub_1.png", "/introClub_2.png"};
+	String str[]=new String[2];
+	int year[]=new int[2];
+	int id;
+	String name_sub;
+	public introEmployment(int key, String sub) {
+		this.id=key;
+		this.name_sub=sub;
+		try {
+			String driverName = "com.mysql.jdbc.Driver"; // 드라이버 이름 지정
+			String DBName = "MirimGuideBook";
+			String dbURL = "jdbc:mysql://10.96.122.177:3306/"+DBName+"?autoReconnect=true&useSSL=false";
+			String SQL = "select year, employ_now from employment1 where id="+id+"";
+			//Class.forName(driverName); // 드라이버 로드
+
+			Connection con  = DriverManager.getConnection(dbURL,"root","mirim546"); // 연결
+			System.out.println("디비연결완료");
+			Statement stmt = con.createStatement();
+			
+			stmt.execute("use "+DBName+";");
+			ResultSet result = stmt.executeQuery(SQL);
+
+			while(result.next()) {
+				for(int i=0;i<2;i++) {
+				str[i] = result.getString(i+1);
+				}
+			}
+			
+			result.close();
+			stmt.close();
+			con.close();	
+		}catch(SQLException sqle) {
+			System.out.println("SQLException: "+sqle.getMessage());
+			System.out.println("SQLState: "+sqle.getSQLState());
+		}
+		
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 400);
+		setVisible(true);
+		setLocationRelativeTo(null); //창 중앙에 띄우기
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		contentPane.setLayout(null);
+		getContentPane().add(contentPane);
+		
+		for(int i=0; i<2; i++) {
+			intro[i]=new JLabel("");
+			introImg[i] = new ImageIcon(this.getClass().getResource(img[i])).getImage();
+			intro[i].setIcon(new ImageIcon(introImg[i]));
+			intro[i].setBounds(10, 120*(i+1), 260, 70);
+			contentPane.add(intro[i]);
+		}
+		for(int i=0;i<2;i++) {
+			System.out.println(str[i]);
+			dbIntro[i]=new JLabel(str[i]);
+			dbIntro[i].setBounds(230, 120*(i+1), 300, 70);
+			contentPane.add(dbIntro[i]);
+		}
+		introTitle=new JLabel("");
+		introTitle.setBounds(100,8,200,100);
+		Image title = new ImageIcon(this.getClass().getResource("/subIntro.png")).getImage();
+		contentPane.add(introTitle);
+		introTitle.setIcon(new ImageIcon(title));
+		
+		label = new JLabel("");
+		label.setSize(400,400);
+		Image img = new ImageIcon(this.getClass().getResource("/Wallpaper.png")).getImage();
+		contentPane.add(label);
+		label.setIcon(new ImageIcon(img));
+		
+	}
+	
+	public static void main(String[] args) {
+	}
+
+}
